210b5e47b9983b29146a573da1d9b24d7a65534e
rohit chatterjee <strategy.chatterjee@gmail.com>
2014-04-28 22:23:32 +0530

    commit after yeoman

---
+++ app/scripts/main.js
@@ -0,0 +1,18 @@
+/*global Invite, $*/
+
+
+window.Invite = {
+    Models: {},
+    Collections: {},
+    Views: {},
+    Routers: {},
+    init: function () {
+        'use strict';
+        console.log('Hello from Backbone!');
+    }
+};
+
+$(document).ready(function () {
+    'use strict';
+    Invite.init();
+});
